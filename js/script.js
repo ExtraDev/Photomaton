8e138ed74eb3678ed2c7e2243ac31d683801317d
Miguel.LPSBR <miguel.lpsbr@gmail.com>
2019-01-31 16:13:11 +0100

    Photomaton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+/* SAVE De la première image
+
+--------- Photomaton -----------
+Taille image reduite : IMAGE_SIZE / (stepNumber - 1)^2
+Nombre d'image à insert : (stepNumber - 1) * 4
+--------------------------------
+ */
+
+
+const DEFAULT_IMAGE_SIZE = 256;
+
+var stepNumber = 1;
+
+// var image = new Image();
+
+// image.src = "img/macron.jpg";
+
+// var imageSize = image.width;
+
